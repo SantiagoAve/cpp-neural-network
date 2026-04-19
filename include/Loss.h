6b169af0099791df116044604ef6c63dfe18e9b3
Santiago V. E. <santiarivillegase@gmail.com>
2026-04-19 01:48:01 -0300

    Decorated a lil bit :)

--- include/Loss.h
+++ include/Loss.h
@@ -3,9 +3,14 @@
 
 class Loss {
     public:
-        // Mean Squared Error formula.
-        static double mean_squared(const Eigen::MatrixXd & true_values, const Eigen::MatrixXd & predicted_values);
+        /*
+            FORMULAS:
+            1. Mean Squared Error formula.
+            2. Mean Squared Error derivative.
+        */
+        static double mean_squared(const Eigen::MatrixXd & true_values,
+                                   const Eigen::MatrixXd & predicted_values);
 
-        // Mean Squared Error derivative.
-        static Eigen::MatrixXd mean_derivative(const Eigen::MatrixXd & true_values, const Eigen::MatrixXd & predicted_values);
+        static Eigen::MatrixXd mean_derivative(const Eigen::MatrixXd & true_values,
+                                               const Eigen::MatrixXd & predicted_values);
 };
--- src/Loss.cpp
+++ src/Loss.cpp
@@ -1,10 +1,19 @@
 #include "Loss.h"
 
-double Loss::mean_squared(const Eigen::MatrixXd & true_values, const Eigen::MatrixXd & predicted_values) {
+/*
+    FORMULAS:
+*/
+double Loss::mean_squared(const Eigen::MatrixXd & true_values,
+                          const Eigen::MatrixXd & predicted_values) {
     Eigen::MatrixXd value_difference = true_values - predicted_values;
     return value_difference.array().square().mean();
 }
 
-Eigen::MatrixXd Loss::mean_derivative(const Eigen::MatrixXd & true_values, const Eigen::MatrixXd & predicted_values) {
+// Mean Squared Error derivative.
+Eigen::MatrixXd Loss::mean_derivative(const Eigen::MatrixXd & true_values,
+                                      const Eigen::MatrixXd & predicted_values) {
     return -2 * (true_values - predicted_values) / true_values.rows();
 }
+/*
+    -----------------------------------------------------------------------------------------------
+*/
