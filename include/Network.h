2e9b0b997a1653a058409326add55bcf59ad1635
Santiago V. E. <santiarivillegase@gmail.com>
2026-04-19 01:43:11 -0300

    Added network backward to both Network.h and Network.cpp

--- include/Network.h
+++ include/Network.h
@@ -1,15 +1,38 @@
 #pragma once
 #include <vector>
 #include "Layer.h"
+#include "Loss.h"
 
 class Network {
     public:
-        // Constructor or class. Initializes a Network with a list (Vector)
-        // of prebuilt Layers.
+        /*
+            NETWORK CONSTRUCTOR:
+            Initializes a Network. Currently, Layer creation is tied to user,
+            future updates will allow Layer creation to be done here.
+        */
         Network(const std::vector<Layer> & layers);
 
         // Forward function that executes forward in each layer.
+        /*
+            FORWARD ACTION:
+            Funcion that executes forward on each layer, providing a final output,
+            starts in the first layer, ends in the last one.
+        */
         Eigen::MatrixXd net_forward(const Eigen::MatrixXd & input);
+
+        /*
+            BACKWARD ACTION:
+            Function that executes backward on each layer, correcting biases and
+            weights, beginning from the end and moving backwards.
+        */
+       void net_backward(const Eigen::MatrixXd & true_values,
+                         const Eigen::MatrixXd & pred_values, double learning_rate);
+
     private:
+        /*
+            INNER VARIABLES:
+            These variables store useful information about the network.
+        */
         std::vector<Layer> layers;
+        size_t num_layers;
 };
--- src/Network.cpp
+++ src/Network.cpp
@@ -1,11 +1,36 @@
 #include "Network.h"
 
-Network::Network(const std::vector<Layer> & layers) : layers(layers) {}
+/*
+    NETWORK CONSTRUCTOR:
+*/
+Network::Network(const std::vector<Layer> & layers) : layers(layers), num_layers(layers.size()) {}
+/*
+    -----------------------------------------------------------------------------------------------
+*/
 
+/*
+    FORWARD & BACKWARD:
+*/
 Eigen::MatrixXd Network::net_forward(const Eigen::MatrixXd & input) {
-    Eigen::MatrixXd cur_output = input;
+    Eigen::MatrixXd cur_output = input; // This works as an iterator.
+
     for (auto & layer : layers) {
         cur_output = layer.forward(cur_output);
     }
+    
     return cur_output;
 }
+
+void Network::net_backward(const Eigen::MatrixXd & true_values,
+                           const Eigen::MatrixXd & pred_values, double learning_rate) {
+    // This is the initial '2 * (a^L - y^L)' that you see normally in calculus.
+    // From here, you backpropagate.
+    Eigen::MatrixXd propag_loss_grad = Loss::mean_derivative(true_values, pred_values);
+
+    for (size_t i = this->num_layers - 1; i >= 0; i--) {
+        propag_loss_grad = this->layers[i].backward(propag_loss_grad, learning_rate);
+    }
+}
+/*
+    -----------------------------------------------------------------------------------------------
+*/
