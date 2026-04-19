69dacc0444701e9762db3cce66dce3fcfe37bc54
Santiago V. E. <santiarivillegase@gmail.com>
2026-04-19 00:57:56 -0300

    Added backward to Layer.h and Layer.cpp

--- include/Layer.h
+++ include/Layer.h
@@ -4,20 +4,48 @@
 
 class Layer {
     public:
-        // Constructor or class. Initializes a Layer with info about input
-        // and output size, plus its activation method.
+        /*
+            LAYER CONSTRUCTOR:
+            Initializes a Layer. Creates a weight matrix of size Output*Input, creates
+            a bias matrix of side Output and uses one of two activations.
+        */
         Layer(int input_size, int output_size, const std::string & activation = "Sigmoid");
 
-        // Forward function that returns a Matrix of dimensions Output*Bach.
-        // Each component is corresponding to the activation of a Neuron.
+        /*
+            FORWARD ACTION:
+            Funcion that returns an output matrix. Said matrix is of size Output*Bach,
+            were each component is the specific activation of a Neuron. Bach ...
+        */
         Eigen::MatrixXd forward(const Eigen::MatrixXd & input);
 
-        // Backward function needs to be implemented too!
+        /*
+            BACKWARD ACTION:
+            Function that updates weights and biases to a more fitting value. Uses the
+            gradient descent principle, so it uses chain rule of the error formula.
+        */
+        Eigen::MatrixXd backward(const Eigen::MatrixXd & propag_loss_grad, double learning_rate);
+
     private:
-        Eigen::MatrixXd weights; // (output * input in size)
-        Eigen::VectorXd biases; // (column/row vector)
-        std::string activation_function; // (Sigmoid or ReLU)
-        // Activation functions:
+        /*
+            INNER VARIABLES:
+            These variables are used to store information of the layer. They are mostly
+            used for backward, like my_z and my_a or prev_input.
+        */
+        Eigen::MatrixXd weights; // Member' Output*Input in size.
+        Eigen::VectorXd biases;
+        Eigen::MatrixXd my_z; // Pre-activation matrix value.
+        Eigen::MatrixXd my_a;
+        Eigen::MatrixXd prev_input;
+        std::string activation_function; // Can be Sigmoid or ReLU.
+
+        /*
+            ACTIVATION FUNCTIONS:
+            These are activation functions and their derivatives, meaning they store how
+            each Neuron is activated and what their derivative is like.
+        */
         Eigen::MatrixXd sigmoid(const Eigen::MatrixXd & z);
+        Eigen::MatrixXd sigmoid_derivative(const Eigen::MatrixXd & z);
         Eigen::MatrixXd relu(const Eigen::MatrixXd & z);
+        Eigen::MatrixXd relu_derivative(const Eigen::MatrixXd & z);
+
 };
--- src/Layer.cpp
+++ src/Layer.cpp
@@ -1,30 +1,77 @@
 #include "Layer.h"
 #include <random>
 
+/*
+    LAYER CONSTRUCTOR:
+*/
 Layer::Layer(int input_size, int output_size, const std::string & activation) {
-    // Initializes weights matrix with small random values, biases vector
-    // with zeros and assings the activation function.
+    // 0.01 avoids to have an untrainnable Neural Network.
     this->weights = Eigen::MatrixXd::Random(output_size, input_size) * 0.01;
     this->biases = Eigen::VectorXd::Zero(output_size);
     this->activation_function = activation;
 }
 
+/*
+    FORWARD & BACKWARD:
+*/
 Eigen::MatrixXd Layer::forward(const Eigen::MatrixXd & input) {
-    // Calculates the basic "z = w * x + b" and applies the layer's activation
-    // function.
-    Eigen::MatrixXd z = (this->weights * input).colwise() + this->biases;
+    // These variables are going to be used for backward later on.
+    this->prev_input = input;
+    this->my_z = (this->weights * input).colwise() + this->biases;
 
     if (this->activation_function == "Sigmoid") {
-        return this->sigmoid(z);
+        this->my_a = this->sigmoid(this->my_z);
     } else {
-        return this->relu(z);
+        this->my_a = this->relu(this->my_z);
     }
+
+    return this->my_a;
 }
 
+Eigen::MatrixXd Layer::backward(const Eigen::MatrixXd & propag_loss_grad, double learning_rate) {
+    // I know it's not the best name, but this is supposed to represent what in calculus you
+    // found as 'dL/dZ = [dL/dA * dA/dZ]'.
+    Eigen::MatrixXd derivative_z;
+
+    // This is what chain rule is translated into.
+    if (this->activation_function == "Sigmoid") {
+        derivative_z = propag_loss_grad.array() * sigmoid_derivative(this->my_z).array();
+    } else {
+        derivative_z = propag_loss_grad.array() * relu_derivative(this->my_z).array();
+    }
+
+    // In this case, this is for representing 'dL/dW = [dL/dA * dA/dZ] * dZ/dW' or for biases,
+    // 'dL/dB = [dL/dA * dA/dZ] * dZ/dB'. Notice how '[dL/dA * dA/dZ]' is 'derivative_z'.
+    Eigen::MatrixXd derivative_w = derivative_z * this->prev_input.transpose();
+    Eigen::MatrixXd derivative_b = derivative_z.rowwise().sum();
+
+    this->weights -= learning_rate * derivative_w;
+    this->biases -= learning_rate * derivative_b;
+
+    // Keep in mind 'derivative_z' size is Output*Bach, and 'weights' size is Output*Input,
+    // therefore, transposition is necessary for 'weights'.
+    return this->weights.transpose() * derivative_z;
+}
+
+/*
+    SIGMOID FUNCTION:
+*/
 Eigen::MatrixXd Layer::sigmoid(const Eigen::MatrixXd & z) {
     return 1 / ( 1 + ( -z.array() ).exp() );
 }
 
+Eigen::MatrixXd Layer::sigmoid_derivative(const Eigen::MatrixXd & z) {
+    Eigen::MatrixXd sigmoid_result = this->sigmoid(z);
+    return sigmoid_result.array() * (1 - sigmoid_result.array());
+}
+
+/*
+    RELU FUNCTION:
+*/
 Eigen::MatrixXd Layer::relu(const Eigen::MatrixXd & z) {
     return z.array().max(0);
+}
+
+Eigen::MatrixXd Layer::relu_derivative(const Eigen::MatrixXd & z) {
+    return (z.array() > 0).cast<double>();
 }
